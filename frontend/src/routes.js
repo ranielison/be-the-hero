2851821b1d012bd98070da6e7903c0e66f1bdf97
Ranielison <ranielisonsoares2@gmail.com>
2020-03-26 23:41:35 -0300

    Finalizando layout da pagina Register

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon/index';
+import Register from './pages/Register/index';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Logon}/>
+        <Route path="/" component={Logon} exact/>
+        <Route path="/register" component={Register}/>
       </Switch>
     </BrowserRouter>
   );
